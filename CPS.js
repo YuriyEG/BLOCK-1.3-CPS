d11c02f24394fd98bc0dd7716a64a327aeefa384
YuriyEG <yuriygermanovich0202@gmail.com>
2023-07-05 14:45:42 +0300

    mobile-is-done

---
+++ CPS.js
@@ -0,0 +1,7 @@
+function show() {
+    var hidden = document.getElementById('more');
+    var moreText = document.getElementById('more-text');
+    moreText.style.display = 'none';
+    hidden.style.display = 'inline';
+    
+}
