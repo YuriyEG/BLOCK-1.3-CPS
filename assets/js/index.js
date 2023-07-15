64e2ca625b225368d62437ccc7bcc77a3e6d723b
YuriyEG <yuriygermanovich0202@gmail.com>
2023-07-16 01:00:01 +0300

    index.js edited

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,61 +1,52 @@
+// Показ скрытого текста
+
 function show() {
-    var hidden = document.getElementById('more');
+    
     var moreText = document.getElementById('more-text');
-    var pict = document.getElementById('pict');
     moreText.style.display = 'none';
-    hidden.style.display = 'inline';
+    var more = document.getElementById('more');
+    more.style.display = 'inline';
  
-        
-    
-    
-    
-    
 };
 
+// Показ и скрытие бокового меню с кнопки
 function showbar() {
-    var hidden2 = document.getElementById('sidebar');
 
-    if (hidden2.style.left !== '0px') {
-        hidden2.style.left = '0px';
-        hidden2.style.transition = 'left 0.5s linear';
+    var sidebar = document.getElementById('sidebar');
 
-        let modal = document.getElementById('saidbar__mask');
-        modal.style.display = 'block';
+    if (sidebar.style.left !== '0px') {
 
+        sidebar.style.left = '0px';
+        sidebar.style.transition = 'left 0.5s linear';
+        sidebar.style.boxShadow = '0 -50px 65px 0 rgba(0, 0, 0, 0.385)';
+        let sidebarMask = document.getElementById('saidbar__mask');
+        sidebarMask.style.display = 'block';
         var root = document.getElementById('root');
         root.style.position = 'fixed';
-        console.log(root.style.position);
+   
     } else {
-        hidden2.style.left = '-400px';
-        hidden2.style.transition = 'left 0.3s linear';
-        var hidden3 = document.getElementById('saidbar__mask');
-        
-        hidden3.style.display = 'none';
-        var saidbar = document.getElementById('sidebar');
 
+        sidebar.style.left = '-320px';
+        sidebar.style.boxShadow = '0 0 0 0 rgba(0, 0, 0, 0)';
+        sidebar.style.transition = 'left 0.3s linear';
+        var saidbarMask = document.getElementById('saidbar__mask');
+        saidbarMask.style.display = 'none';
         var root = document.getElementById('root');
         root.style.position = 'relative';
 
-        
-
-
-    }
-
-
-    
-    
-    
+    } 
 };
+
+// Скрытие бокового меню при клике на заблокированную область
 function closebar() {
 
-        var hidden3 = document.getElementById('saidbar__mask');
-        
-        hidden3.style.display = 'none';
+        var saidbarMask = document.getElementById('saidbar__mask');
+        saidbarMask.style.display = 'none';
         var saidbar = document.getElementById('sidebar');
         saidbar.style.left = '-400px';
         saidbar.style.transition = 'left 0.5s linear';
-
         var root = document.getElementById('root');
         root.style.position = 'relative';
-        
+        saidbar.style.boxShadow = '0 0 0 0 black';
+          
 };
