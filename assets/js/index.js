7d58d1a772779629271a0c16f99fa8e335c6a925
YuriyEG <yuriygermanovich0202@gmail.com>
2023-07-20 22:11:18 +0300

    slider add

--- assets/js/index.js
+++ assets/js/index.js
@@ -87,3 +87,26 @@ mainSummary.addEventListener( 'click', function () {
     hiddenText.style.display = 'inline';
     
 });
+
+
+const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
