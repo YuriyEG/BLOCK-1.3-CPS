2b88ce5369426b26d4ac93c9d8618841ce2c443f
YuriyEG <yuriygermanovich0202@gmail.com>
2023-07-20 23:18:23 +0300

    slider add2

--- assets/js/index.js
+++ assets/js/index.js
@@ -97,6 +97,7 @@ const swiper = new Swiper('.swiper', {
         // If we need pagination
         pagination: {
           el: '.swiper-pagination',
+          clickable: 'true'
         },
       
 
